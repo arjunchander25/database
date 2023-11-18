118e7b5c4e66be71f5be3eccbbe8171f7877c1b4
arjunchander25 <143799534+arjunchander25@users.noreply.github.com>
2023-11-18 20:56:38 +0530

    Create Calculate Special Bonus.sql

---
+++ leetcode_sql/Calculate Special Bonus.sql
@@ -0,0 +1,9 @@
+SELECT 
+    employee_id,
+    CASE 
+        WHEN employee_id % 2!=0 AND name  not LIKE 'M%' THEN salary
+        ELSE 0
+    END AS bonus
+FROM 
+    Employees
+order by employee_id;
